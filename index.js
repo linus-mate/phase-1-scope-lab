5900bc1d4dfe424ae5b2d30f72d2d1b4ee8ecfbe
linus mutugi mate <mlynux@gmail.com>
2022-08-17 22:47:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "junet";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "sankok";
+}
